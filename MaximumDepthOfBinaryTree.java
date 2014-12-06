cf79fdaac5d523987fe4c9094d72d3fa1704712d
dclab <diekey@qq.com>
2014-12-06 20:57:57 +0800

    maximum depth of binary tree

---
+++ MaximumDepthOfBinaryTree.java
@@ -0,0 +1,24 @@
+package cn.edu.sjtu.zzang;
+
+import java.util.ArrayList;
+
+public class MaximumDepthOfBinaryTree {
+	public static void main(String[] args) {
+		MaximumDepthOfBinaryTree bt = new MaximumDepthOfBinaryTree();
+		TreeNode root = new TreeNode(3);
+		TreeNode l1 = new TreeNode(2);
+		TreeNode r1 = new TreeNode(5);
+		TreeNode r2 = new TreeNode(4);
+		TreeNode r3 = new TreeNode(6);
+		root.left = l1;
+		root.right = r1;
+		r1.left = r2;
+		r1.right = r3;
+		System.out.println(bt.maxDepth(root));
+	}
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+        	return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
