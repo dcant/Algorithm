a593561806c709831789df31c67c6e9be00fee96
dclab <diekey@qq.com>
2014-12-07 14:56:57 +0800

    path sum

---
+++ PathSum.java
@@ -0,0 +1,29 @@
+package cn.edu.sjtu.zzang;
+
+import org.w3c.dom.Node;
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+        	return false;
+        if (root.left == null && root.right == null) {
+        	if (root.val == sum)
+        		return true;
+        	return false;
+        } else 
+        	return checker(root, 0, sum);
+    }
+    
+    private boolean checker(TreeNode node, int csum, int sum) {
+    	if (node.left == null && node.right == null) {
+    		if (csum + node.val == sum)
+    			return true;
+    		return false;
+    	} else if (node.left == null)
+    		return checker(node.right, csum + node.val, sum);
+    	else if (node.right == null)
+    		return checker(node.left, csum + node.val, sum);
+    	else
+    		return checker(node.left, csum + node.val, sum) || checker(node.right, csum + node.val, sum);
+    }
+}
