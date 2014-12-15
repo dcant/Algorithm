6c7591b9bfa859a99841285090d4b25c59b70492
dclab <diekey@qq.com>
2014-12-15 21:16:22 +0800

    undirected graph

---
+++ UndirectedGraphNode.java
@@ -0,0 +1,10 @@
+package cn.edu.sjtu.zzang;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UndirectedGraphNode {
+	int label;
+	List<UndirectedGraphNode> neighbors;
+	UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+}
