cb07cb0fd8a3c79cd3220a6818909c04e1dc71bd
dclab <diekey@qq.com>
2014-12-19 22:16:40 +0800

    linked list cycle

---
+++ LinkedListCycle.java
@@ -0,0 +1,16 @@
+package cn.edu.sjtu.zzang;
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+    	if (head == null)
+    		return false;
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+        	if (slow == fast)
+        		return true;
+        	slow = slow.next;
+        	fast = fast.next.next;
+        }
+        return false;
+    }
+}
