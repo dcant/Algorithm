9b6507c98a9ee2c3cad64632c2f38bbc928fd403
dclab <diekey@qq.com>
2014-12-22 20:42:53 +0800

    changed some code

--- ReorderList.java
+++ ReorderList.java
@@ -9,11 +9,8 @@ public class ReorderList {
  
 			//use a fast and slow pointer to break the link to two parts.
 			while (fast != null && fast.next != null && fast.next.next!= null) {
-				//why need third/second condition?
-				System.out.println("pre "+slow.val + " " + fast.val);
 				slow = slow.next;
 				fast = fast.next.next;
-				System.out.println("after " + slow.val + " " + fast.val);
 			}
  
 			ListNode second = slow.next;
